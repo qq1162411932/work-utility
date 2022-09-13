23b8bdc35e4de00edc92f69cac7a75ae13e7e87f
wu <1162411932@qq.com>
2022-09-13 18:08:44 +0800

    feat: 测试数据db

---
+++ core/db/20220913.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `test` (
+    `name`  varchar(255) DEFAULT NULL,
+    `value` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `test`.`test`(`name`, `value`) VALUES ('1', '1');
